c9c97e9a375eb911009532db841246e7beb88000
Joe Kopena <tjkopena@bellerophonmobile.com>
2013-08-01 13:45:37 -0400

    TextBitmap moved around.

--- samples/10-RocketHaxeBasicGame/src/Menu.hx
+++ samples/10-RocketHaxeBasicGame/src/Menu.hx
@@ -3,6 +3,8 @@ package;
 import com.rocketshipgames.haxe.ui.Screen;
 import com.rocketshipgames.haxe.device.Display;
 
+import com.rocketshipgames.haxe.gfx.text.TextBitmap;
+
 import flash.display.Bitmap;
 import openfl.Assets;
 
@@ -16,11 +18,21 @@ class Menu
   public function new():Void
   {
     super();
-    var bitmap = new Bitmap (Assets.getBitmapData ("assets/sprites.png"));
-    addChild(bitmap);
-                
-    bitmap.x = (Display.width - bitmap.width) / 2;
-    bitmap.y = (Display.height - bitmap.height) / 2;
+
+    // Put the RocketHaxe logo in the bottom right
+    var rockethaxe =
+      new Bitmap(Assets.getBitmapData("assets/rockethaxe-100x30.png"));
+    rockethaxe.x = Display.width - (rockethaxe.width+8);
+    rockethaxe.y = Display.height - (rockethaxe.height+8);
+    addChild(rockethaxe);
+
+    var title =
+      TextBitmap.makeBitmap("RocketHaxe Demo Shooter",
+                            { size: 32 });
+    title.x = (Display.width - title.width)/2;
+    title.y = (Display.height/5) - (title.height/2);
+    addChild(title);
+
     // end new
   }
 
