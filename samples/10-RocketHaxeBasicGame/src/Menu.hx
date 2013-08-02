ed6bc49e29ce080712caed9086a8217222f4548a
Joe Kopena <tjkopena@bellerophonmobile.com>
2013-08-01 22:33:03 -0400

    Play button.

--- samples/10-RocketHaxeBasicGame/src/Menu.hx
+++ samples/10-RocketHaxeBasicGame/src/Menu.hx
@@ -1,9 +1,13 @@
 package;
 
+import com.rocketshipgames.haxe.ui.ScreenManager;
 import com.rocketshipgames.haxe.ui.Screen;
+import com.rocketshipgames.haxe.ui.Panel;
 import com.rocketshipgames.haxe.device.Display;
 
 import com.rocketshipgames.haxe.gfx.text.TextBitmap;
+import com.rocketshipgames.haxe.gfx.widgets.DecoratedBitmap;
+import com.rocketshipgames.haxe.ui.widgets.TextBitmapButton;
 
 import flash.display.Bitmap;
 import openfl.Assets;
@@ -22,15 +26,14 @@ class Menu
     // Put the title up center
     var title =
       TextBitmap.makeBitmap("RocketHaxe Demo Shooter",
-                            { size: 32 });
+                            { size: 32, color: 0xffffff });
     title.x = (Display.width - title.width)/2;
     title.y = (Display.height/5) - (title.height/2);
     addChild(title);
 
 
     // Put the RocketHaxe logo in the bottom right
-    var rockethaxe =
-      com.rocketshipgames.haxe.gfx.widgets.DecoratedBitmap.makeBitmap
+    var rockethaxe = DecoratedBitmap.makeBitmap
       (Assets.getBitmapData("assets/rockethaxe-100x30.png"),
        { padding: 2, bgcolor: 0xffffffff });
     rockethaxe.x = Display.width - (rockethaxe.width+8);
@@ -40,14 +43,15 @@ class Menu
 
     // Put a Play button in the middle
     var play =
-      new com.rocketshipgames.haxe.ui.widgets.TextBitmapButton
-      (function():Void { haxe.Log.trace("PLAY"); }, "Play",
-       { size: 18, color: 0xff000000, bgcolor: 0xffffffff,
+      new TextBitmapButton
+      (function():Void { ScreenManager.show("game"); },
+       "Play",
+       { size: 18, color: 0xdf000000, bgcolor: 0xdfffffff,
          padding: 8, paddingLeft: 24, paddingRight: 24,
          borderWidth: 2, borderColor: 0xff000000,
        },
        { },
-       { color: 0xffffffff, bgcolor: 0xff000000, borderColor: 0xffffffff },
+       { color: 0xff000000, bgcolor: 0xffffffff, borderColor: 0xff000000 },
        { color: 0xffffffff, bgcolor: 0xff000000, borderColor: 0xffffffff }
        );
     play.x = (Display.width - play.width)/2;
@@ -56,6 +60,24 @@ class Menu
     play.show();
 
     // end new
+  }
+
+
+  //--------------------------------------------------------------------
+  //----------------------------------------------------
+  public override function show(?opts:Dynamic):Void
+  {
+    com.rocketshipgames.haxe.device.Mouse.enable();
+    super.show(opts);
+    // end show
+  }
+
+  //----------------------------------------------------
+  public override function hide(onComplete:PanelNotifier, ?opts:Dynamic):Void
+  {
+    com.rocketshipgames.haxe.device.Mouse.disable();
+    super.hide(onComplete, opts);
+    // end show
   }
 
   // end Menu
