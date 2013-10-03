a1295a52bfefc4b3dbe642b1324cc376ddff6125
Joe Kopena <tjkopena@bellerophonmobile.com>
2013-10-03 01:22:29 -0400

    Viewport tracker now has margins.

--- com/rocketshipgames/haxe/world/behaviors/ViewportTrackerComponent.hx
+++ com/rocketshipgames/haxe/world/behaviors/ViewportTrackerComponent.hx
@@ -6,7 +6,7 @@ import com.rocketshipgames.haxe.component.ComponentHandle;
 import com.rocketshipgames.haxe.gfx.Viewport;
 
 import com.rocketshipgames.haxe.physics.PhysicsCapabilities;
-import com.rocketshipgames.haxe.physics.Position2D;
+import com.rocketshipgames.haxe.physics.Extent2D;
 
 
 class ViewportTrackerComponent
@@ -14,19 +14,36 @@ class ViewportTrackerComponent
 {
 
   private var viewport:Viewport;
-  private var position:Position2D;
+  private var extent:Extent2D;
 
   private var active:Bool;
+
+  private var marginLeft:Float;
+  private var marginRight:Float;
+  private var marginTop:Float;
+  private var marginBottom:Float;
+
+  private var left:Float;
+  private var right:Float;
+  private var top:Float;
+  private var bottom:Float;
 
 
   //--------------------------------------------------------------------
   private function new(viewport:Viewport):Void
   {
+
     this.viewport = viewport;
+
+    marginLeft = marginRight = marginTop = marginBottom = 0.0;
+
+    left = right = top = bottom = 0.0;
+
     // end new
   }
 
-  public static function create(viewport:Viewport, ?opts:Dynamic):ViewportTrackerComponent
+  public static function create(viewport:Viewport,
+                                ?opts:Dynamic):ViewportTrackerComponent
   {
     var x = new ViewportTrackerComponent(viewport);
     x.activate(opts);
@@ -39,6 +56,31 @@ class ViewportTrackerComponent
   public function activate(?opts:Dynamic):Void
   {
     active = true;
+
+    if (opts == null)
+      return;
+
+
+    var d:Dynamic;
+    if ((d = Reflect.field(opts, "margin")) != null) {
+      marginLeft = marginRight = marginTop = marginBottom =
+        (Std.is(d, String)) ? Std.parseFloat(d) : d;
+    }
+
+    if ((d = Reflect.field(opts, "marginLeft")) != null) {
+      marginLeft = (Std.is(d, String)) ? Std.parseFloat(d) : d;
+    }
+    if ((d = Reflect.field(opts, "marginRight")) != null) {
+      marginRight = (Std.is(d, String)) ? Std.parseFloat(d) : d;
+    }
+    if ((d = Reflect.field(opts, "marginTop")) != null) {
+      marginTop = (Std.is(d, String)) ? Std.parseFloat(d) : d;
+    }
+    if ((d = Reflect.field(opts, "marginBottom")) != null) {
+      marginBottom = (Std.is(d, String)) ? Std.parseFloat(d) : d;
+    }
+
+    // end activate
   }
 
   public function deactivate():Void
@@ -50,8 +92,14 @@ class ViewportTrackerComponent
   //----------------------------------------------------
   public function attach(container:ComponentHandle):Void
   {
-    position = cast(container.find(PhysicsCapabilities.CID_POSITION2D),
-                    Position2D);
+    extent = cast(container.find(PhysicsCapabilities.CID_EXTENT2D),
+                    Extent2D);
+
+    left = extent.left();
+    right = extent.right();
+    top = extent.top();
+    bottom = extent.bottom();
+
     // end attach
   }
 
@@ -62,18 +110,33 @@ class ViewportTrackerComponent
   //--------------------------------------------------------------------
   public function update(elapsed:Int):Void
   {
+
     if (!active)
       return;
 
-    if (viewport.x > position.x-25)
-      viewport.x = position.x-25;
-    else if (viewport.x+viewport.width < position.x+25)
-      viewport.x = (position.x+25)-viewport.width;
+    if (left != extent.left() || right != extent.right() ||
+        top != extent.top() || bottom != extent.bottom()) {
 
-    if (viewport.y > position.y-25)
-      viewport.y = position.y-25;
-    else if (viewport.y+viewport.height < position.y+25)
-      viewport.y = (position.y+25)-viewport.height;
+      left = extent.left();
+      right = extent.right();
+      top = extent.top();
+      bottom = extent.bottom();
+
+      var x = viewport.x;
+      var y = viewport.y;
+
+      if (x + viewport.width < extent.right() + marginRight)
+        x = extent.right() + marginRight - viewport.width;
+      else if (x > extent.left() - marginLeft)
+        x = extent.left() - marginLeft;
+
+      if (y + viewport.height < extent.bottom() + marginBottom)
+        y = extent.bottom() + marginBottom - viewport.height;
+      else if (y > extent.top() - marginTop)
+        y = extent.top() - marginTop;
+
+      viewport.set(x, y);
+    }
 
     // end update
   }
